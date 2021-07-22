1ea7a5124f9a8579fa2e7be92e7813c21f5b4ccc
quzq <quzyam@gmail.com>
2021-07-22 11:16:18 +0900

    add stage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -45,45 +47,75 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+// List shuffleArray(List items) {
+//   var random = new Random();
+
+//   // Go through all elements.
+//   for (var i = items.length - 1; i > 0; i--) {
+//     // Pick a pseudorandom number according to the list length
+//     var n = random.nextInt(i + 1);
+
+//     var temp = items[i];
+//     items[i] = items[n];
+//     items[n] = temp;
+//   }
+
+//   return items;
+// }
+
 class _MyHomePageState extends State<MyHomePage> {
   List<int> _numbers = [];
-  int? _selectedIndex = null;
+  List<bool> _visibilities = [];
+  int? _selectedIndex;
+  bool _showAnswer = false;
+  String _debug = '';
+  int _score = 0;
+  bool isStarting = false;
+  _MyHomePageState() {
+    //_init();
+  }
 
-  void _initNumbers() {
+  List chunkArray(list, int length) {
+    List chunks = [];
+    for (var i = 0; i < list.length; i += length) {
+      chunks
+          .add(list.sublist(i, i + 2 > list.length ? list.length : i + length));
+    }
+    return chunks;
+  }
+
+  void _init() {
     List<int> seed = new List.generate(8, (i) => i);
     setState(() {
       _numbers = seed + seed;
+      //_numbers = shuffleArray(_numbers) as List<int>;
+      _visibilities = new List.generate(_numbers.length, (i) => true);
+      _score = seed.length;
+      _selectedIndex = null;
     });
-  }
-
-  void _selectIndex(int? index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  List _chunk(list, int length) {
-    List chunkedList = [
-      [1, 2, 3, 4],
-      [5, 6, 7, 8],
-      [1, 2, 3, 4],
-      [5, 6, 7, 8]
-    ];
-    return chunkedList;
   }
 
   @override
   Widget build(BuildContext context) {
-    _initNumbers();
+    final Size deviceSize = MediaQuery.of(context).size;
 
     Widget getButtonMatrix(List<int> numbers) {
-      var rows = _chunk(numbers, 4).map((childArray) {
-        List<int> array = childArray;
+      int columnsLength = 4;
+      var rows =
+          chunkArray(numbers, columnsLength).asMap().entries.map((rowEntry) {
+        int rowIndex = rowEntry.key;
+        List<int> rowValue = rowEntry.value;
         return Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: array.map((i) {
+            children: rowValue.asMap().entries.map((colEntry) {
+              int colIndex = rowIndex * columnsLength + colEntry.key;
+              int colValue = colEntry.value;
+              bool colVisibility = _visibilities[colIndex];
               return OutlinedButton(
-                child: Text(i.toString()),
+                child: Text(
+                    _showAnswer || _selectedIndex == colIndex || !colVisibility
+                        ? colValue.toString()
+                        : colVisibility.toString()),
                 style: OutlinedButton.styleFrom(
                   primary: Colors.black,
                   shape: RoundedRectangleBorder(
@@ -91,7 +123,33 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   side: const BorderSide(),
                 ),
-                onPressed: () {},
+                onPressed: !_visibilities[colIndex]
+                    ? null
+                    : () {
+                        setState(() {
+                          if (_selectedIndex != null) {
+                            int lastValue = _numbers[_selectedIndex ?? 99];
+                            _debug = colValue.toString() +
+                                ':' +
+                                lastValue.toString();
+                            if (colValue == lastValue &&
+                                _selectedIndex != colIndex) {
+                              _visibilities = _visibilities
+                                  .asMap()
+                                  .entries
+                                  .map((visibilityEntry) {
+                                return visibilityEntry.key == colIndex ||
+                                        visibilityEntry.key == _selectedIndex
+                                    ? false
+                                    : visibilityEntry.value;
+                              }).toList();
+                            } else {
+                              _score--;
+                            }
+                          }
+                          _selectedIndex = colIndex;
+                        });
+                      },
               );
             }).toList());
       });
@@ -102,18 +160,32 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+          title: Text(
+        widget.title + ' ' + _score.toString() + ' ' + _debug,
+      )),
       body: Center(
-        child: getButtonMatrix(_numbers),
+        child: _score > 0 ? getButtonMatrix(_numbers) : Text('game over'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _initNumbers,
-        tooltip: 'Increment',
+        onPressed: _showAnswer
+            ? null
+            : () {
+                _init();
+                setState(() {
+                  _showAnswer = true;
+                });
+                Timer.periodic(
+                  Duration(seconds: 1),
+                  (Timer t) {
+                    setState(() {
+                      _showAnswer = false;
+                    });
+                  },
+                );
+              },
+        tooltip: 'start',
         child: Icon(Icons.sync),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
